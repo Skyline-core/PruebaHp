3a77c58461947e0f378120664c7d0f0ac901e001
Skyline-core <angelah967@gmail.com>
2018-04-23 15:17:44 -0500

    Estructura3

---
+++ app/Http/Controllers/FrontController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FrontController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+}
